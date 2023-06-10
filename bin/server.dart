e4a721dd7eaa867a74cd0ad706e8cc87369ee3ef
uiwwsw <uiwwsw@icloud.com>
2023-06-10 12:02:22 +0900

    chore: change port

--- bin/server.dart
+++ bin/server.dart
@@ -35,7 +35,7 @@ void main(List<String> args) async {
   final handler = Pipeline().addMiddleware(logRequests()).addHandler(_router);
 
   // For running in containers, we respect the PORT environment variable.
-  final port = int.parse(Platform.environment['PORT'] ?? '8080');
+  final port = int.parse(Platform.environment['PORT'] ?? '80');
   final server = await serve(handler, ip, port);
   print('Server listening on port ${server.port}');
 }
