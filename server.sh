8184bbffbabf46f7897ce7dd5bacbce460ccb071
uiwwsw <uiwwsw@icloud.com>
2023-06-12 15:26:07 +0900

    chore: path

--- server.sh
+++ server.sh
@@ -1,4 +1,4 @@
-sudo chmod 755 bin./server.dart
+sudo chmod 755 bin/server.dart
 sudo iptables -I OUTPUT 1 -p tcp --dport 80 -j ACCEPT
 sudo iptables -I INPUT 1 -p tcp --dport 80 -j ACCEPT
 nohup sudo dart run bin/server.dart &
