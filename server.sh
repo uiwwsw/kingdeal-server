a0adb4ffa823acf8fdb409c8dca446a2f72e9399
uiwwsw <uiwwsw@icloud.com>
2023-06-14 12:18:59 +0900

    test

--- server.sh
+++ server.sh
@@ -3,6 +3,6 @@ if [ -f program.pid ]; then
 fi
 
 sudo chmod 755 bin/server.dart
-sudo iptables -I OUTPUT 1 -p tcp --dport 80 -j ACCEPT
-sudo iptables -I INPUT 1 -p tcp --dport 80 -j ACCEPT
-nohup sudo dart run bin/server.dart > nohup.out 2>&1 & echo $! > program.pid
+&&sudo iptables -I OUTPUT 1 -p tcp --dport 80 -j ACCEPT
+&&sudo iptables -I INPUT 1 -p tcp --dport 80 -j ACCEPT
+&&nohup sudo dart run bin/server.dart > nohup.out 2>&1 & echo $! > program.pid
