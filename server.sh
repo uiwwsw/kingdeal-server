326d67b2e1153cde218395eb519d20a87e91e3e6
uiwwsw <uiwwsw@icloud.com>
2023-06-26 22:13:16 +0900

    test

--- server.sh
+++ server.sh
@@ -1,5 +1,5 @@
 if [ -f program.pid ]; then
-  sudo pkill -9 -ef 'dart run '$(dirname $0)'/bin/server.dart'
+  sudo pkill -9 -ef 'dart run /home/ubuntu/kingdeal-server/bin/server.dart'
 fi
 
-sudo chmod 755 $(dirname $0)/bin/server.dart &&sudo iptables -I OUTPUT 1 -p tcp --dport 80 -j ACCEPT &&sudo iptables -I INPUT 1 -p tcp --dport 80 -j ACCEPT &&nohup sudo dart run $(dirname $0)/bin/server.dart > nohup.out 2>&1 &
+sudo chmod 755 /home/ubuntu/kingdeal-server/bin/server.dart &&sudo iptables -I OUTPUT 1 -p tcp --dport 80 -j ACCEPT &&sudo iptables -I INPUT 1 -p tcp --dport 80 -j ACCEPT &&nohup sudo dart run /home/ubuntu/kingdeal-server/bin/server.dart > nohup.out 2>&1 &
